ba298a1a3f81a4cebe2a5cfbafb9f1363d54d602
yanyige <232218719@qq.com>
2015-11-02 09:27:42 +0800

    imqq

--- js/index_js.js
+++ js/index_js.js
@@ -15,5 +15,25 @@ $(document).ready(function(){
 
     $.myscroll(".e6b");
 
+    jQuery.imqq = function(){
+        $(window).scroll(function(){
+            console.log(document.all.content2.offsetWidth);
+            console.log($("#eg7_wrap").offset().top);
+            var t1 =  $("#eg7_wrap").offset().top - $(window).scrollTop();
+            t1 = t1 * 0.03;
+            var pos = "50% "+t1+"px";
+            $(".eg7_bg1").css({"background-position":pos});
+            var t2 = $("#content2").offset().top - $(window).scrollTop();
+            t2 = t2 * 0.03;
+            var pos = "50% "+t2+"px";
+            $(".eg7_bg2").css({"background-position":pos});
+            var t3 = $("#content3").offset().top - $(window).scrollTop();
+            t3 = t3 * 0.03;
+            var pos = "50% "+t3+"px";
+            $(".eg7_bg3").css({"background-position":pos});
+        });
+    }
+
+    $.imqq();
 });
 
